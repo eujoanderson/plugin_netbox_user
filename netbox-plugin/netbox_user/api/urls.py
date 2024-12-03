cf14ece0e63bd119860b0ca722ab3471318dfa20
Joanderson <82622160+eujoanderson@users.noreply.github.com>
2024-12-03 01:40:50 -0300

    Update urls.py

--- netbox-plugin/netbox_user/api/urls.py
+++ netbox-plugin/netbox_user/api/urls.py
@@ -4,14 +4,14 @@ from . import views
 app_name = 'plugin-user'
 
 router = NetBoxRouter()
-router.register('plugin-user', views.UserListViewSet)
-router.register('pluginuserrule', views.UserListRuleViewSet, basename='pluginuserrule')
-router.register('resourceslist', views.ResourcesListViewSet, basename='resourceslist')
-router.register('environmentlist', views.EnvironmentListViewSet, basename='environmentlist')
-router.register('grouplist', views.GroupsListViewSet, basename='grouplist')
-router.register('groups', views.GroupsListViewSet, basename='groups-list')
-router.register('approverlist', views.ApproverListViewSet, basename='approverlist')
-router.register('sectorlist', views.SectorListViewSet, basename='sectorlist')
+router.register('plugin-user', views.UserListViewSet, basename='userlist')
+router.register('resourceaccess', views.UserListRuleViewSet, basename='resourceaccess')
+router.register('resources', views.ResourcesListViewSet, basename='resources')
+
+router.register('environment', views.EnvironmentListViewSet, basename='environment')
+router.register('groups', views.GroupsListViewSet, basename='groups')
+router.register('approver', views.ApproverListViewSet, basename='approver')
+router.register('sector', views.SectorListViewSet, basename='sector')
 router.register('resourcegroups', views.ResourceGroupsListViewSet, basename='resourcegroups')
 
 
